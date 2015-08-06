67c3dbaa91b02e864c86aa3062e4fd26a9385812
s-nakajima <nakajimashouhei@gmail.com>
2015-08-06 10:42:49 +0900

    Initial commit

---
+++ Controller/RoomSpaceController.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * RoomSpace Controller
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('RoomSpaceAppController', 'RoomSpace.Controller');
+
+/**
+ * RoomSpace Controller
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\RoomSpace\Controller
+ */
+class RoomSpacesController extends RoomSpaceAppController {
+
+/**
+ * use model
+ *
+ * @var array
+ */
+	//public $uses = array();
+
+/**
+ * use component
+ *
+ * @var array
+ */
+	//public $components = array();
+
+/**
+ * index
+ *
+ * @return void
+ */
+	public function index() {
+	}
+
+/**
+ * view
+ *
+ * @return void
+ */
+	public function view() {
+	}
+
+/**
+ * add
+ *
+ * @return void
+ */
+	public function add() {
+	}
+
+/**
+ * edit
+ *
+ * @return void
+ */
+	public function edit() {
+	}
+
+/**
+ * delete
+ *
+ * @return void
+ */
+	public function delete() {
+	}
+}
