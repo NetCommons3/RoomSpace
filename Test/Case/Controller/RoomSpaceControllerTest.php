8d6a2655e59e7384cd69fe2f781213cb12e21c03
s-nakajima <nakajimashouhei@gmail.com>
2015-08-13 03:59:07 +0900

    AllRoomSpaceTestコード追加

---
+++ Test/Case/Controller/RoomSpaceControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * RoomSpaceController Test Case
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('YAControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * RoomSpaceController Test Case
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\RoomSpace\Test\Case\Controller
+ */
+class RoomSpaceControllerTest extends YAControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+	}
+
+}
